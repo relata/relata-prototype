e00429a655cd9e1a3d9eca1d7b4dbb830aa0c262
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-07-27 14:02:00 -0700

    Run Prettier on project

--- src/app.js
+++ src/app.js
@@ -1,22 +1,20 @@
-const path = require('path');
-const favicon = require('serve-favicon');
-const compress = require('compression');
-const helmet = require('helmet');
-const cors = require('cors');
-const logger = require('./logger');
+const path = require("path");
+const favicon = require("serve-favicon");
+const compress = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
+const logger = require("./logger");
 
-const feathers = require('@feathersjs/feathers');
-const configuration = require('@feathersjs/configuration');
-const express = require('@feathersjs/express');
+const feathers = require("@feathersjs/feathers");
+const configuration = require("@feathersjs/configuration");
+const express = require("@feathersjs/express");
 
+const middleware = require("./middleware");
+const services = require("./services");
+const appHooks = require("./app.hooks");
+const channels = require("./channels");
 
-
-const middleware = require('./middleware');
-const services = require('./services');
-const appHooks = require('./app.hooks');
-const channels = require('./channels');
-
-const authentication = require('./authentication');
+const authentication = require("./authentication");
 
 const app = express(feathers());
 
@@ -28,13 +26,12 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+app.use(favicon(path.join(app.get("public"), "favicon.ico")));
 // Host the public folder
-app.use('/', express.static(app.get('public')));
+app.use("/", express.static(app.get("public")));
 
 // Set up Plugins and providers
 app.configure(express.rest());
-
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
