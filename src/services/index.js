7da28c37a5a0869f36d95212920129522a35eac6
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-07-29 18:08:00 -0700

    Add relations service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const users = require("./users/users.service.js");
+const relations = require("./relations/relations.service.js");
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(users);
+  app.configure(relations);
 };
---
+++ src/services/relations/relations.class.js
@@ -0,0 +1,3 @@
+const { Service } = require("feathers-nedb");
+
+exports.Relations = class Relations extends Service {};
