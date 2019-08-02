c904a7556d042740611472408d53998a651c60ee
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-08-01 20:29:00 -0700

    Validate relation

--- src/services/relations/relations.hooks.js
+++ src/services/relations/relations.hooks.js
@@ -1,13 +1,68 @@
-const { setNow } = require("feathers-hooks-common");
+const { keep, required, setNow, validate } = require("feathers-hooks-common");
+
+// Array against which to validate relation_type field
+const validRelationTypes = [
+  "absence",
+  "criticism",
+  "critique",
+  "extension",
+  "omission",
+  "rejoinder"
+];
+// Simple validator for relations
+const validateRelation = (formValues, context) => {
+  const { relation_type } = formValues;
+  if (relation_type && !validRelationTypes.includes(relation_type)) {
+    return {
+      relation_type: `relation_type must be one of the following: ${validRelationTypes.join(
+        ", "
+      )}`
+    };
+  }
+};
 
 module.exports = {
   before: {
     all: [],
     find: [],
     get: [],
-    create: [setNow("created_at", "updated_at")],
-    update: [setNow("updated_at")],
-    patch: [setNow("updated_at")],
+    create: [
+      required(
+        "relation_type",
+        "relation_from",
+        "relation_from.type",
+        "relation_from.title",
+        "relation_from.author",
+        "relation_to",
+        "relation_to.type",
+        "relation_to.title",
+        "relation_to.author"
+      ),
+      validate(validateRelation),
+      keep("relation_type", "annotation", "relation_from", "relation_to"),
+      setNow("created_at", "updated_at")
+    ],
+    update: [
+      required(
+        "relation_type",
+        "relation_from",
+        "relation_from.type",
+        "relation_from.title",
+        "relation_from.author",
+        "relation_to",
+        "relation_to.type",
+        "relation_to.title",
+        "relation_to.author"
+      ),
+      validate(validateRelation),
+      keep("relation_type", "annotation", "relation_from", "relation_to"),
+      setNow("updated_at")
+    ],
+    patch: [
+      validate(validateRelation),
+      keep("relation_type", "annotation", "relation_from", "relation_to"),
+      setNow("updated_at")
+    ],
     remove: []
   },
 
