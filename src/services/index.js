ef9315453ca5f7da581a79cae2ad0ddab5e7e510
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-08-09 13:18:00 -0700

    Add works service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require("./users/users.service.js");
 const relations = require("./relations/relations.service.js");
+const works = require("./works/works.service.js");
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(users);
   app.configure(relations);
+  app.configure(works);
 };
