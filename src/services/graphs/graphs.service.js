704e43fba38256832ac3547f7e08102d09a6f208
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-09-13 22:00:00 -0700

    Generate graphs service

---
+++ src/services/graphs/graphs.service.js
@@ -0,0 +1,19 @@
+// Initializes the `graphs` service on path `/graphs`
+const { Graphs } = require("./graphs.class");
+const hooks = require("./graphs.hooks");
+
+module.exports = function(app) {
+  const paginate = app.get("paginate");
+
+  const options = {
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use("/graphs", new Graphs(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service("graphs");
+
+  service.hooks(hooks);
+};
