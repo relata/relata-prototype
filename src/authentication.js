bbbf27a6282baf395cc934442ff772ea1386416d
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-11-01 01:56:00 -0800

    Implement working OAuth via GitHub

--- src/authentication.js
+++ src/authentication.js
@@ -2,13 +2,27 @@ const {
   AuthenticationService,
   JWTStrategy
 } = require("@feathersjs/authentication");
-const { LocalStrategy } = require("@feathersjs/authentication-local");
-const { expressOauth } = require("@feathersjs/authentication-oauth");
+const {
+  OAuthStrategy,
+  expressOauth
+} = require("@feathersjs/authentication-oauth");
+
+class GitHubStrategy extends OAuthStrategy {
+  async getEntityData(profile) {
+    const baseData = await super.getEntityData(profile);
+
+    return {
+      ...baseData,
+      githubUsername: profile.login
+    };
+  }
+}
 
 module.exports = app => {
   const authentication = new AuthenticationService(app);
 
   authentication.register("jwt", new JWTStrategy());
+  authentication.register("github", new GitHubStrategy());
 
   app.use("/authentication", authentication);
   app.configure(expressOauth());
--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -1,53 +1,19 @@
 const { authenticate } = require("@feathersjs/authentication").hooks;
-const {
-  alterItems,
-  discard,
-  keep,
-  required,
-  setNow
-} = require("feathers-hooks-common");
-
-const {
-  hashPassword,
-  protect
-} = require("@feathersjs/authentication-local").hooks;
+const { setNow } = require("feathers-hooks-common");
 
 module.exports = {
   before: {
     all: [],
     find: [authenticate("jwt")],
     get: [authenticate("jwt")],
-    create: [
-      required("name", "email", "password"),
-      alterItems(user => {
-        user.role = "user";
-      }),
-      keep("name", "email", "password", "role"),
-      hashPassword("password"),
-      setNow("created_at", "updated_at")
-    ],
-    update: [
-      required("name", "email", "password"),
-      discard("role"),
-      hashPassword("password"),
-      authenticate("jwt"),
-      setNow("updated_at")
-    ],
-    patch: [
-      discard("role"),
-      hashPassword("password"),
-      authenticate("jwt"),
-      setNow("updated_at")
-    ],
+    create: [setNow("created_at", "updated_at")],
+    update: [setNow("updated_at")],
+    patch: [authenticate("jwt"), setNow("updated_at")],
     remove: [authenticate("jwt")]
   },
 
   after: {
-    all: [
-      // Make sure the password field is never sent to the client
-      // Always must be the last hook
-      protect("password")
-    ],
+    all: [],
     find: [],
     get: [],
     create: [],
