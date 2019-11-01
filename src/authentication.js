3e008beff337979cf8d4497c019d9739bef3f402
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-11-01 01:29:18 -0800

    Generate OAuth configuration for auth

--- src/authentication.js
+++ src/authentication.js
@@ -9,7 +9,6 @@ module.exports = app => {
   const authentication = new AuthenticationService(app);
 
   authentication.register("jwt", new JWTStrategy());
-  authentication.register("local", new LocalStrategy());
 
   app.use("/authentication", authentication);
   app.configure(expressOauth());
