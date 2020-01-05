5a07e2d9b6fcbb1b19f973be1a8ffba5026894c9
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-01-05 00:34:51 -0800

    Remove existing Feathers app for replacement

--- test/authentication.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-const app = require("../src/app");
-
-describe("authentication", () => {
-  it("registered the authentication service", () => {
-    assert.ok(app.service("authentication"));
-  });
-});
