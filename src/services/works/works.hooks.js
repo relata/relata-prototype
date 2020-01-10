4ebbecc1799382511db63ab662bf9649eca904f4
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-01-10 15:01:58 -0800

    Gracefully reject duplicate works by fuzzy match scoring

--- src/services/works/works.hooks.js
+++ src/services/works/works.hooks.js
@@ -1,7 +1,42 @@
 const { sequelizeConvert } = require("feathers-hooks-common");
+const { compareTwoStrings } = require("string-similarity");
+
+const { makeCitations } = require("../graphs/utilities/citations");
 
 const convert = {
   data: "json"
+};
+
+// Very rough duplicate-detection function that performs fuzzy comparison of
+// one bibliographic citation to another. Not very sophisticated; just a guard
+// against simple cases
+const detectDuplicates = (workA, workB) => {
+  const workACitation = makeCitations({ data: workA }).bibliography;
+  const workBCitation = makeCitations({ data: workB }).bibliography;
+  const score = compareTwoStrings(workACitation, workBCitation);
+  return score > 0.67;
+};
+
+// Hook to reject a work if it appears to be a duplicate of an existing work in
+// the database; by setting context.result here, we short-circuit the CREATE
+// call to the database and simply send back the existing work as it is
+const rejectDuplicateWork = async context => {
+  const { app, data } = context;
+  const worksService = app.service("works");
+  const results = await worksService.find({
+    $limit: 30000,
+    paginate: {
+      // Set an extreme upper limit to avoid killing the app
+      max: 30000
+    }
+  });
+  for (existingWork of results) {
+    if (detectDuplicates(data, existingWork.data)) {
+      context.result = existingWork;
+      break;
+    }
+  }
+  return context;
 };
 
 // Despite documentation, an error crops up when adding sequelizeConvert hook
@@ -11,7 +46,7 @@ module.exports = {
     all: [],
     find: [sequelizeConvert(convert)],
     get: [sequelizeConvert(convert)],
-    create: [],
+    create: [rejectDuplicateWork],
     update: [sequelizeConvert(convert)],
     patch: [sequelizeConvert(convert)],
     remove: [sequelizeConvert(convert)]
