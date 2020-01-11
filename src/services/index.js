8e53013f1978cd677dd1a1909ebdf776491ba8c4
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-01-10 18:15:19 -0800

    Create empty config service

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require("./users/users.service.js");
 const works = require("./works/works.service.js");
 const relations = require("./relations/relations.service.js");
 const graphs = require("./graphs/graphs.service.js");
+const config = require("./config/config.service.js");
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(users);
   app.configure(works);
   app.configure(relations);
   app.configure(graphs);
+  app.configure(config);
 };
