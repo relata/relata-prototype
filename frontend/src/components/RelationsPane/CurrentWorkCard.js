b197d7b7b94fc4328b1ee2abdaecbb41915c1c3c
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-01-13 00:04:25 -0800

    Add Linkify for clickable DOIs/URLs in CurrentWorkCard

--- frontend/src/components/RelationsPane/CurrentWorkCard.js
+++ frontend/src/components/RelationsPane/CurrentWorkCard.js
@@ -1,13 +1,26 @@
 import React from "react";
+import Linkify from "react-linkify";
 
 import Card from "react-bootstrap/Card";
+
+// Workaround to enable Linkify to target _new within React-Bootstrap
+// components
+const componentDecorator = (href, text, key) => (
+  <a href={href} key={key} target="_blank">
+    {text}
+  </a>
+);
 
 const CurrentWorkCard = props => {
   const { currentWork } = props;
   return (
     <Card border="dark" className="mb-3">
       <Card.Body>
-        <Card.Text>{currentWork.bibliography}</Card.Text>
+        <Card.Text>
+          <Linkify componentDecorator={componentDecorator}>
+            {currentWork.bibliography}
+          </Linkify>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
