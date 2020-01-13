82918c321aded02ade9c1477eeecee1f1ab6027b
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-01-13 14:38:37 -0800

    Reorganize RelationsPane

---
+++ frontend/src/components/RelationsPane/RelationCard.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const RelationCard = props => {
+  const { relation, selectWork } = props;
+
+  var annotation;
+  if (relation.annotation) {
+    var annotationAuthor = relation.annotationAuthor ? (
+      <b className="relation-lead">{relation.annotationAuthor}</b>
+    ) : (
+      ""
+    );
+    annotation = (
+      <Card.Footer>
+        <Card.Text>
+          {annotationAuthor} {relation.annotation}
+        </Card.Text>
+      </Card.Footer>
+    );
+  } else {
+    annotation = "";
+  }
+
+  return (
+    <div>
+      <Card
+        className="relation-card mt-3"
+        onClick={() => selectWork(relation.workTo.id)}
+        style={{ borderLeftColor: relation.color }}
+      >
+        <Card.Body>
+          <Card.Text>
+            <b className="relation-lead" style={{ color: relation.color }}>
+              {relation.type}
+            </b>{" "}
+            {relation.workTo.bibliography}
+          </Card.Text>
+        </Card.Body>
+        {annotation}
+      </Card>
+    </div>
+  );
+};
+
+export default RelationCard;
