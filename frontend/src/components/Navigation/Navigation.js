a19d180accd757b74db4e86e8788cfb3ac2a345f
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-01-17 06:45:02 -0800

    Initial pass at reimplementing My Contributions modal in React

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-const Navigation = () => {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg" fluid="true">
-      <Navbar.Brand href="/">Relata</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse>
-        <Nav>
-          <Nav.Link
-            href="https://culanth.org/engagements/relata"
-            target="_blank"
-          >
-            About
-          </Nav.Link>
-          <Nav.Link target="_blank">My Contributions</Nav.Link>
-          <Nav.Link target="_blank">Sign Up</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
-};
+import SearchModal from "./SearchModal";
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { searchModalIsOpen: false };
+  }
+
+  toggleSearchModal = () => {
+    const { searchModalIsOpen } = this.state;
+    this.setState({
+      searchModalIsOpen: !searchModalIsOpen
+    });
+  };
+
+  render() {
+    const { searchModalIsOpen } = this.state;
+    const { relataConfig, selectWork } = this.props;
+
+    return (
+      <Navbar bg="dark" variant="dark" expand="lg" fluid="true">
+        <Navbar.Brand href="/">Relata</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link
+              href="https://culanth.org/engagements/relata"
+              target="_blank"
+            >
+              About
+            </Nav.Link>
+            <Nav.Link onClick={this.toggleSearchModal}>
+              My Contributions
+            </Nav.Link>
+            <Nav.Link target="_blank">Sign Up</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <SearchModal
+          show={searchModalIsOpen}
+          relataConfig={relataConfig}
+          selectWork={selectWork}
+          toggleSearchModal={this.toggleSearchModal}
+        />
+      </Navbar>
+    );
+  }
+}
 
 export default Navigation;
---
+++ frontend/src/components/Navigation/SearchModal.js
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-bootstrap/Modal";
+
+import client from "../../feathers";
+
+import { makeCitations } from "../RelationsPane/AddRelationModal/SelectWork/utilities/citations";
+
+class SearchModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userRelations: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUserRelations(1);
+  }
+
+  getUserRelations = userId => {
+    const relationsService = client.service("relations");
+
+    relationsService
+      .find({
+        query: {
+          $limit: 1000,
+          $sort: {
+            createdAt: -1
+          },
+          userId: userId,
+          expand: true
+        }
+      })
+      .then(results => {
+        const relations = results.data;
+        this.setState({ userRelations: relations });
+      });
+  };
+
+  deleteRelation = async relationId => {
+    const relationsService = client.service("relations");
+    try {
+      const result = relationsService.remove(relationId);
+      this.getUserRelations(1);
+    } catch (error) {
+      return;
+    }
+  };
+
+  cancelModal = () => {
+    const { toggleSearchModal } = this.props;
+
+    // Close modal
+    toggleSearchModal();
+  };
+
+  render() {
+    const { relataConfig, show, selectWork } = this.props;
+    const { userRelations } = this.state;
+
+    let relationListItems;
+    if (userRelations.length > 0) {
+      relationListItems = userRelations.map(relation => {
+        const workFrom = {
+          ...relation.workFrom,
+          ...makeCitations(relation.workFrom)
+        };
+        const workTo = {
+          ...relation.workTo,
+          ...makeCitations(relation.workTo)
+        };
+        let color;
+        if (relataConfig.types) {
+          const typeConfig = relataConfig.types[relation.type];
+          if (typeConfig) {
+            color = typeConfig.color;
+          } else {
+            color = "";
+          }
+        }
+        return (
+          <ListGroup.Item
+            key={relation.id}
+            onClick={() => selectWork(relation.workFrom.id)}
+            style={{ borderLeft: `0.25rem solid ${color}` }}
+            action
+          >
+            <Button
+              variant="danger"
+              className="float-right"
+              size="sm"
+              onClick={() => this.deleteRelation(relation.id)}
+            >
+              Delete
+            </Button>
+            <span className="relation-lead" style={{ color: color }}>
+              {relation.type}
+            </span>
+            {workFrom.citation} → {workTo.citation}
+          </ListGroup.Item>
+        );
+      });
+    } else {
+      relationListItems = "";
+    }
+
+    return (
+      <Modal show={show} onHide={this.cancelModal} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Contributions</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>You have contributed the following relations. Click to view:</p>
+          <Card>
+            <ListGroup
+              variant="flush"
+              style={{ overflow: "scroll", maxHeight: "24rem" }}
+            >
+              {relationListItems}
+            </ListGroup>
+          </Card>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-secondary" onClick={this.cancelModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default SearchModal;
