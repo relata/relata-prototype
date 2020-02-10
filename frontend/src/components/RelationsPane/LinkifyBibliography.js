89f4c670627504c7585ead236adb32e8a434a3ac
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-02-09 23:44:45 -0800

    Add line breaks anywhere within a Linkify URL; add Linkify to RelationCard

--- frontend/src/components/RelationsPane/LinkifyBibliography.js
+++ frontend/src/components/RelationsPane/LinkifyBibliography.js
@@ -4,7 +4,14 @@ import Linkify from "react-linkify";
 // Workaround to enable Linkify to target _new within React-Bootstrap
 // components
 const componentDecorator = (href, text, key) => (
-  <a href={href} key={key} rel="noopener noreferrer" target="_blank">
+  <a
+    href={href}
+    key={key}
+    rel="noopener noreferrer"
+    target="_blank"
+    onClick={event => event.stopPropagation()}
+    style={{ wordBreak: "break-all", msWordBreak: "break-all" }}
+  >
     {text}
   </a>
 );
