2312a415b6cb393aa081ac82af1c2c437cac2c47
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-02-11 23:47:33 -0800

    Update app.js/configs to discard extraneous import/secret

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-const path = require("path");
-const favicon = require("serve-favicon");
 const compress = require("compression");
 const helmet = require("helmet");
 const cors = require("cors");
@@ -32,7 +30,6 @@ app.use(cors());
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get("public"), "favicon.ico")));
 
 // Host the public folder
 app.use("/", express.static(app.get("public")));
@@ -50,7 +47,7 @@ app.use(
     }),
     resave: false,
     saveUninitialized: false,
-    secret: app.get("authentication").sessionSecret
+    secret: app.get("authentication").secret
   })
 );
 
